f7f6c6a0491d8f73f23d0090a8a1457baedd9bb5
Abdul <abduldudekula199@gmail.com>
2025-08-08 22:57:51 +0530

    initial application setup with React

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './welcome.jsx';
+
+function App() {
+    return ( <
+        div className = 'App' >
+
+        <
+        header className = 'header-App' >
+        <
+        img src = { logo }
+        className = 'App-logo'
+        alt = 'logo'/>
+        <
+        p >
+        Edit < code> src / App.js </code>and save to reload.</p >
+        < a className = 'App-link'
+        href = 'https://reactjs.org'
+        target = '_blank'
+        rel = 'noopener noreferrer' > Learn react
+         </a> 
+        </header>  <Greeting / >
+        </div>
+    );
+};
+
+export default App;
---
+++ my-app/src/welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Greeting =()=>{
+let Name="abdul";
+    return(
+        <div>
+            <h1>My name</h1>
+            <p>Names</p>
+            <button>click here</button>
+        </div>
+    );
+};
+export default Greeting;
